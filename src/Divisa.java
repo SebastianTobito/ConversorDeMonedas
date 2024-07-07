ba8f8bed286607f9639f16881b955e96d0852809
Sebastian Tobito <sebastiantobito@gmail.com>
2024-07-06 19:05:30 -0500

    creando la clase Divisa con sus getters y setters, creando DivisaApi con los datos a traer de la api, creando la conexion http en la clase ConsultaDivisa y en la clase Principal creando el cascaron de la aplicación

--- src/Divisa.java
+++ src/Divisa.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public record Divisa() {
+import com.google.gson.annotations.SerializedName;
+
+public class Divisa {
+    @SerializedName("base_code")
+    private String monedaBase;
+    @SerializedName("target_code")
+    private String monedaAConvertir;
+    @SerializedName("conversion_rate")
+    private String radioDeConversion;
+    @SerializedName("conversion_result")
+    private String resultadoConversion;
+
+    public Divisa(String monedaBase, String monedaAConvertir, String radioDeConversion, String resultadoConversion) {
+        this.monedaBase = monedaBase;
+        this.monedaAConvertir = monedaAConvertir;
+        this.radioDeConversion = radioDeConversion;
+        this.resultadoConversion = resultadoConversion;
+    }
+
+    public String getMonedaBase() {
+        return monedaBase;
+    }
+
+    public void setMonedaBase(String monedaBase) {
+        this.monedaBase = monedaBase;
+    }
+
+    public String getMonedaAConvertir() {
+        return monedaAConvertir;
+    }
+
+    public void setMonedaAConvertir(String monedaAConvertir) {
+        this.monedaAConvertir = monedaAConvertir;
+    }
+
+    public String getRadioDeConversion() {
+        return radioDeConversion;
+    }
+
+    public void setRadioDeConversion(String radioDeConversion) {
+        this.radioDeConversion = radioDeConversion;
+    }
+
+    public String getResultadoConversion() {
+        return resultadoConversion;
+    }
+
+    public void setResultadoConversion(String resultadoConversion) {
+        this.resultadoConversion = resultadoConversion;
+    }
 }
---
+++ src/DivisaApi.java
@@ -0,0 +1,2 @@
+public record DivisaApi(String monedaBase, String monedaAConvertir, String radioDeConversion, String resultadoConversion ) {
+}
